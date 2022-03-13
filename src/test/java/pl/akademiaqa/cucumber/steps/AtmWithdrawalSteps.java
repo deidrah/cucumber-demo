4bb8a56e8fbd79a099be66107876d13930c5c9c7
Deidrah <dominikastasik93@gmail.com>
2022-03-13 18:29:50 +0100

    add steps definiton

---
+++ src/test/java/pl/akademiaqa/cucumber/steps/AtmWithdrawalSteps.java
@@ -0,0 +1,26 @@
+package pl.akademiaqa.cucumber.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AtmWithdrawalSteps {
+
+    @Given("I have {int} PLN in my account")
+    public void i_have_pln_in_my_account(Integer int1) {
+
+    }
+    @When("I request {int} PLN")
+    public void i_request_pln(Integer int1) {
+
+    }
+    @Then("{int} PLN should be dispensed")
+    public void pln_should_be_dispensed(Integer int1) {
+
+    }
+    @Then("{int} PLN should stay on my account")
+    public void pln_should_stay_on_my_account(Integer int1) {
+
+    }
+
+}
